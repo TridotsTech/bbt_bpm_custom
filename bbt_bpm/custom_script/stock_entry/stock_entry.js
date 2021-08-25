bb8dcea30b61d0ff337830105a033d35b1b5fe95
jitendra.r <jitendra.r@indictranstech.com>
2021-08-25 11:29:50 +0530

    Fix-Add Delivery warehouse on sales order

--- bbt_bpm/custom_script/stock_entry/stock_entry.js
+++ bbt_bpm/custom_script/stock_entry/stock_entry.js
@@ -3,11 +3,13 @@ frappe.ui.form.on("Stock Entry", {
 		if (frm.doc.sales_order_ref){
 			frm.set_value("stock_entry_type", "Stock Reservation")
 			frm.trigger("hide_add_delete_option")
+			frm.trigger("to_warehouse")
 		}
 
 		if (frm.doc.quotation_ref) {
 			frm.set_value("stock_entry_type", "Stock Reservation")
 			frm.trigger("hide_add_delete_option")
+			frm.trigger("to_warehouse")
 		}
 	},
 
@@ -38,5 +40,19 @@ frappe.ui.form.on("Stock Entry", {
 		$('[data-fieldname="items"]').find('.grid-duplicate-row').hide()
 		$('[data-fieldname="items"]').find('.pull-right').hide()
 		$('[data-fieldname="items"]').find('.text-muted').hide()
+	},
+
+	to_warehouse: function(frm)	{
+		var wh = frm.doc.from_warehouse.split("-");
+		var whstr = wh[0]+"-"+wh[1]+"-"+" Reserved"
+		frm.set_query("to_warehouse", function() {
+			return {
+				filters: [
+					["Warehouse", 'company', '=', frm.doc.company],
+					["Warehouse", "is_group", "=",0],
+					["Warehouse", "name", "like", whstr + "%"]
+				]
+			};
+		});
 	}
 })
