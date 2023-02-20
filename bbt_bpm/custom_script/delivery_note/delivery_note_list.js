d7ba6bd2e7cc796e1d199d358789b0056beff93f
Swapnil pawar <dnyaneshwar.p@indictranstech.com>
2023-02-20 18:15:56 +0530

    add delivered flag in std filter delivery note

--- bbt_bpm/custom_script/delivery_note/delivery_note_list.js
+++ bbt_bpm/custom_script/delivery_note/delivery_note_list.js
@@ -6,16 +6,17 @@ frappe.listview_settings['Delivery Note'] = {
 			return [__("Return"), "darkgrey", "is_return,=,Yes"];
 		} else if (doc.status === "Closed") {
 			return [__("Closed"), "green", "status,=,Closed"];
-		} else if (doc.delivered == 1) {
-			return [__("Delivered"), "green"];
+		} else if (doc.delivered == 1 && doc.status === 'To Bill') {
+			// return [__("Delivered"), "green"];
+			return [__("Delivered"), "orange", "status,=,To Bill|delivered,=,1"];
 		} 
 		// else if (flt(doc.per_billed, 2) < 100) {
 		// 	return [__("Goods-In-Transit"), "orange", "per_billed,<,100"];
 		// } 
 		else if (flt(doc.delivered, 2) == 100) {
 			return [__("Completed"), "green", "per_billed,=,100"];
-		} else if (doc.status === 'To Bill'){
-			return [__("Goods-In-Transit"), "orange", "status,=,To Bill"];
+		} else if (doc.status === 'To Bill' && doc.delivered == 0){
+			return [__("Goods-In-Transit"), "green", "status,=,To Bill|delivered,=,0"];
 		}
 
 	},
