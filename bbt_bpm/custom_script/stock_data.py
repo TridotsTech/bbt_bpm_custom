97a5b10d563e3a5c7c6a91f8d7ab8e318ccdbd74
Swapnil pawar <dnyaneshwar.p@indictranstech.com>
2023-08-18 18:47:58 +0530

    change response format

--- bbt_bpm/custom_script/stock_data.py
+++ bbt_bpm/custom_script/stock_data.py
@@ -10,11 +10,20 @@ def stock_data():
 	all_data = []
 
 	for pl in prices_list:
-		print(pl)
 		data = frappe.db.sql("""
-		SELECT sum(BN.actual_qty) as actual_qty,BN.item_name as item_name,BN.item_code as item_code,IP.price_list as price_list from `tabBin` as BN INNER JOIN `tabItem Price` AS IP ON BN.item_code = IP.item_code where BN.warehouse='{0}' and IP.price_list = "{1}" group by BN.item_code""".format(warehouse,pl), as_dict=True)
+		SELECT sum(BN.actual_qty) as actual_qty,BN.item_name as item_name,BN.item_code as item_code,IP.price_list as price_list,IP.price_list_rate as price_list_rate from `tabBin` as BN INNER JOIN `tabItem Price` AS IP ON BN.item_code = IP.item_code where BN.warehouse='{0}' and IP.price_list = "{1}" group by BN.item_code""".format(warehouse,pl), as_dict=True)
 		
-		print(data)
-		all_data.extend(data)
+		for row in data:
+			existing_item = next((item for item in all_data if item["item_code"] == row["item_code"]), None)
+			if existing_item:
+				existing_item["price_list"][row["price_list"]] = row["price_list_rate"]
+			else:
+				item_data = {
+				"actual_qty": row["actual_qty"],
+				"item_name": row["item_name"],
+				"item_code": row["item_code"],
+				"price_list": {row["price_list"]: row["price_list_rate"]}
+				}
+				all_data.append(item_data)
 
 	return all_data
